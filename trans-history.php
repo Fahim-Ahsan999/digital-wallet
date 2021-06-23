5f254a108a1794209d346ed18112e2a6ae8e45db
Ahsan <85099918+Fahim-Ahsan999@users.noreply.github.com>
2021-06-23 14:19:03 +0600

    Add files via upload

---
+++ trans-history.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML Form</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	
+	<h1>Page 1 [Transaction History]</h1>
+    <h3>Digital Wallet</h3>
+    <a href="dw-home.php">1. Home</a> 
+    <a href="trans-history.php">2.Tansaction History</a>
+
+
+   
+
+  
+     <h3> Total Records: </h3>
+       
+
+</form>
+</body>
+
+	
